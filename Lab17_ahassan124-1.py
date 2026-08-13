dfcaa22545058eb4957da9ba1e28b3f3ca4ff21c
untothyself <Ahassan124@student.cscc.edu>
2026-08-13 15:52:21 -0400

    Add Hacker News API requests

--- Lab17_ahassan124-1.py
+++ Lab17_ahassan124-1.py
@@ -8,3 +8,32 @@ Starter Code: Based on the hn_submissions.py example from
 Python Crash Course, Chapter 17.
 Date: August 13, 2026
 """
+
+import requests
+
+
+url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+
+response = requests.get(url, timeout=10)
+
+print(f"Status code: {response.status_code}")
+
+submission_ids = response.json()
+
+submission_dicts = []
+
+for submission_id in submission_ids[:30]:
+    url = (
+        "https://hacker-news.firebaseio.com/v0/item/"
+        f"{submission_id}.json"
+    )
+
+    response = requests.get(url, timeout=10)
+    response_dict = response.json()
+
+    submission_dict = {
+        "title": response_dict["title"],
+        "comments": response_dict.get("descendants", 0),
+    }
+
+    submission_dicts.append(submission_dict)
